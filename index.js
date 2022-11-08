4426239516097c131b6aa07929c3cc3d3360ce26
Shamim Sarkar <shamimibas@gmail.com>
2022-11-08 14:18:02 +0600

    Created api for home page with 3 services data

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.zn49gp5.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const servicesCollection = client.db('slickSmileDB').collection('services');
+        const reviewCollection = client.db('slickSmileDB').collection('review');
+
+// get 3 services data for home page 
+        app.get('/services', async(req, res) => {
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const result = await cursor.limit(3).toArray();
+            res.send(result);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.error(err));
 
 app.get('/', (req, res) => {
     res.send("SLICK SMILE SERVER RUNNING");
